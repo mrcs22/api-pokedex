f1030929957ec2dc2d4b3732bb2cef15b6e614e9
mrcs22 <marcusmoraes2010@hotmail.com>
2021-08-06 22:20:06 -0300

    fix: orm ssl config

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   type: "postgres",
@@ -6,8 +6,13 @@ module.exports = {
   migrationsTableName: "migrations",
   entities: ["dist/entities/*.js"],
   migrations: ["dist/migrations/*.js"],
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   cli: {
     migrationsDir: "src/migrations",
-    entitiesDir: "dist/entities/*.js"
-  }
+    entitiesDir: "dist/entities/*.js",
+  },
 };
