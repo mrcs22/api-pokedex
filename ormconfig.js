c265a4499881aa8b1d9a55c2cbb61d52a4a531d4
mrcs22 <marcusmoraes2010@hotmail.com>
2021-08-12 15:35:57 -0300

    fix: paths to dist directory

--- ormconfig.js
+++ ormconfig.js
@@ -4,8 +4,8 @@ module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
   migrationsTableName: "migrations",
-  entities: ["dist/entities/*.js"],
-  migrations: ["dist/migrations/*.js"],
+  entities: ["dist/src/entities/*.js"],
+  migrations: ["dist/src/migrations/*.js"],
   extra: {
     ssl: {
       rejectUnauthorized: false,
@@ -13,6 +13,6 @@ module.exports = {
   },
   cli: {
     migrationsDir: "src/migrations",
-    entitiesDir: "dist/entities/*.js",
+    entitiesDir: "dist/src/entities/*.js",
   },
 };
